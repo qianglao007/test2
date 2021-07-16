02bcabc9710a771a01c03b89399999417a632829
bernlao <bernlao@tencent.com>
2021-07-16 19:06:48 +0800

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const app = express();
 
 app.get('/', (req, res) => {
-    let token = fs.readFileSync('./config.json').toString()
+    let token = fs.readFileSync('/.tencentcloudbase/wx/cloudbase_access_token').toString()
     let openid = req.get('X-WX-OPENID');
     axios.post(`https://api.weixin.qq.com/wxa/getopendata?openid=${openid}&cloudbase_access_token=${token}`)
         .then(resp => {
